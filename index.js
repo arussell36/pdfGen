a6788a5915d72720e8fecba05c8a60e484a96564
arussell36 <andrewrussell036@gmail.com>
2019-11-16 17:34:24 -0600

    pdf convert installed

--- index.js
+++ index.js
@@ -3,13 +3,21 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+const HTML5ToPDF = require("html5-to-pdf");
+const path = require("path");
+
+
 let starCount = 0;
 inquirer
-  .prompt({
+  .prompt([{
     message: "Enter your GitHub username:",
     name: "username"
-  })
-  .then(function({username}) {
+  },
+  {
+    message: "Preferred Color:",
+    name: "color"
+  }])
+  .then(function({username, color}) {
     const queryUrl = `https://api.github.com/users/${username}`;
     axios.get(queryUrl).then(function(response) {
       // console.log(response);
@@ -48,7 +56,7 @@ inquirer
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Document</title>
 </head>
-<body>
+<body style="background-color:${color}">
       <div>Username: ${user}</div>
       <div>Name: ${name}</div>
       <div><img src="${profileIMG}" /></div>
@@ -63,9 +71,28 @@ inquirer
 </html>
 `;
     })
-    .then(HTMLOutput => {
-      fs.writeFile("index.html", HTMLOutput, () => {
-      });
+    .then(function(HTMLOutput) {
+      fs.writeFile(`${username}.html`, HTMLOutput, () => {
+        const createPDF = async () => {
+        const html5ToPDF = new HTML5ToPDF({
+          inputPath: path.join(__dirname, `./${username}.html`),
+          outputPath: path.join(__dirname, `./${username}.pdf`),
+          // include: [
+          //   path.join(__dirname, "./template.css"),
+          //   // path.join(__dirname, `./${color}.css`)
+          // ],
+          options: { printBackground: true }
+        });
+        await html5ToPDF.start();
+        await html5ToPDF.build();
+        await html5ToPDF.close();
+        console.log("DONE");
+        process.exit(0);
+      };
+    return { html: HTMLOutput, pdf: createPDF() }
+    })
+
+
     });
   });
 });
